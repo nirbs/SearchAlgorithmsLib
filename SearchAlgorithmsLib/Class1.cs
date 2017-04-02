c62ef13b7f1054431871c4987cfcfe702af7b699
NIR BEN SHALOM <nir.benshalom@live.biu.ac.il>
2017-04-02 20:54:44 +0300

    added BFS class. Need to finish it and add maze-searchable.

--- SearchAlgorithmsLib/Class1.cs
+++ SearchAlgorithmsLib/Class1.cs
@@ -11,11 +11,10 @@ namespace SearchAlgorithmsLib
         private List<State<T>> solution;
         public List<State<T>> getSolution() { return solution; }
     }
-//test
 
     public class State<T>
     {
-    private T state;    // the state represented by a string
+    private T state;    // the state represented by a T
     private double cost;    // cost to reach this state (set by a setter)
     private State<T> cameFrom;    // the state we came from to this state (setter)
     public State(T state)   // CTOR
@@ -66,6 +65,46 @@ namespace SearchAlgorithmsLib
             return evaluatedNodes;
         }
         public abstract Solution<T> search(ISearchable<T> searchable);
+        public void addToOpenList(State<T> state, int i)
+        {
+            openList.Enqueue(state, i);
+        }
+        public bool contains(State<T> s) 
+        {
+            if (openList.Contains(s))
+            {
+                return true;
+            }
+            return false;
+        }
     }
-
+    public class BFS<T>: Searcher<T>
+    {
+        public override Solution<T> search(ISearchable<T> searchable){ // Searcher's abstract method overriding
+            int i = 0;
+            addToOpenList(searchable.getInitialState(), i); // inherited from Searcher
+            HashSet<State<T>> closed = new HashSet<State<T>>();
+            while (OpenListSize > 0)
+            {
+                State<T> n = popOpenList(); // inherited from Searcher, removes the best state
+                closed.Add(n);
+                if (n.Equals(searchable.getIGoallState()))
+                    return backTrace(); // private method, back traces through the parents
+                                        // calling the delegated method, returns a list of states with n as a parent
+                List<State<T>> succerssors = searchable.getAllPossibleStates(n);
+                foreach (State<T> s in succerssors)
+                {
+                    if (!closed.Contains(s) && !contains(s))
+                    {
+                        // s.setCameFrom(n); // already done by getSuccessors
+                        i++;
+                        addToOpenList(s, i);
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+    }
 }
