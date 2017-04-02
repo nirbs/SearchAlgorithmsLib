3f8d4dc6cfc49397d3cfedd973af85a932b72fd2
NIR BEN SHALOM <nir.benshalom@live.biu.ac.il>
2017-04-02 22:21:54 +0300

    added mazeSearchable

--- SearchAlgorithmsLib/Class1.cs
+++ SearchAlgorithmsLib/Class1.cs
@@ -15,7 +15,7 @@ namespace SearchAlgorithmsLib
     public class State<T>
     {
     private T state;    // the state represented by a T
-    private double cost;    // cost to reach this state (set by a setter)
+    //private double cost;    // cost to reach this state (set by a setter)
     private State<T> cameFrom;    // the state we came from to this state (setter)
     public State(T state)   // CTOR
     {
@@ -25,6 +25,15 @@ namespace SearchAlgorithmsLib
     {
         return state.Equals(s.state);
     } 
+    public T getState()
+    {
+        return this.state;
+    }
+    public void setCameFrom(State<T> s)
+    {
+        cameFrom = s;
+    }
+    
 }
     public interface ISearchable <T>
     {
@@ -76,6 +85,74 @@ namespace SearchAlgorithmsLib
                 return true;
             }
             return false;
+        }
+    }
+
+    public abstract class Searchable<T> : ISearchable<T>
+    {
+        private State<T> goalState;
+        private State<T> initialState;
+        public Searchable(State<T> initial, State<T> goal)
+        {
+            initialState = initial;
+            goalState = goal;
+        }
+        public abstract List<State<T>> getAllPossibleStates(State<T> s);
+
+        public State<T> getGoalState()
+        {
+            return goalState;
+        }
+
+        public State<T> getInitialState()
+        {
+            return initialState;
+        }
+    }
+
+    public class MazeSearchable : Searchable<MazeLib.Position>
+    {
+        private MazeLib.Maze myMaze;
+        public MazeSearchable(State<MazeLib.Position> initial, State<MazeLib.Position> goal) : base(initial, goal) { };
+        public override List<State<MazeLib.Position>> getAllPossibleStates(State<MazeLib.Position> s)
+        {
+            int x = s.getState().Col;
+            int y = s.getState().Row;
+            List<State<MazeLib.Position>> listush = new List<State<MazeLib.Position>>();
+            if (myMaze[y + 1, x].Equals("Free"))
+            {
+                MazeLib.Position p = new MazeLib.Position (y+1,x);
+            
+                State<MazeLib.Position> down = new State<MazeLib.Position>(p);
+                down.setCameFrom(s);
+                listush.Add(down);
+            }
+            if (myMaze[y - 1, x].Equals("Free"))
+            {
+                MazeLib.Position p = new MazeLib.Position(y - 1, x);
+                State<MazeLib.Position> up = new State<MazeLib.Position>(p);
+                up.setCameFrom(s);
+                listush.Add(up);
+            }
+            if (myMaze[y, x+1].Equals("Free"))
+            {
+                MazeLib.Position p = new MazeLib.Position(y, x+1);
+                State<MazeLib.Position> right = new State<MazeLib.Position>(p);
+                right.setCameFrom(s);
+                listush.Add(right);
+            }
+            if (myMaze[y, x - 1].Equals("Free"))
+            {
+                MazeLib.Position p = new MazeLib.Position(y, x - 1);
+                State<MazeLib.Position> left = new State<MazeLib.Position>(p);
+                left.setCameFrom(s);
+                listush.Add(left);
+            }
+            return listush;
+        }
+        public void setMaze(MazeLib.Maze maze)
+        {
+            myMaze = maze;
         }
     }
     public class BFS<T>: Searcher<T>
